c9aa86dfa1be5907151c4e9d68822e9191c58846
maitrongnhan001 <maitrongnhan.001@gmail.com>
2022-01-04 18:37:44 +0700

    completed fom submit photo

---
+++ src/custom-fields/random-photo-field/random-photo-field.jsx
@@ -0,0 +1,46 @@
+
+import RandomPhoto from 'components/randomPhoto/random-photo';
+import { ErrorMessage } from 'formik';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { FormFeedback, FormGroup, Label } from 'reactstrap';
+
+function RandomPhotoField(props) {
+  const { field, form, label } = props;
+  const { name, value, onBlur } = field;
+  const { errors, touched } = form;
+  const showError = errors[name] && touched[name];
+
+  const handleImageUrlChange = (newImageUrl) => {
+    form.setFieldValue(name, newImageUrl)
+  }
+
+  return (
+    <FormGroup>
+      {label && <Label for={name}>{label}</Label>}
+
+      <RandomPhoto
+        name={name}
+        imageUrl={value}
+        onImageUrlChange={handleImageUrlChange}
+        onRandomButtonBlur={onBlur}
+      />
+
+      <div className={showError ? 'is-invalid' : ''}></div>
+      <ErrorMessage name={name} component={FormFeedback} />
+    </FormGroup>
+  );
+}
+
+RandomPhotoField.propTypes = {
+    field: PropTypes.object.isRequired,
+    form: PropTypes.object.isRequired,
+  
+    label: PropTypes.string,
+  };
+  
+  RandomPhotoField.defaultProps = {
+    label: '',
+  }
+
+export default RandomPhotoField;
